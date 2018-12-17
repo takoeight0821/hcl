e6747f332c847215ebf1b05c3761f67f69370b74
Yuya Kono <takohati0821@gmail.com>
2018-12-17 22:46:41 +0900

    update

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,10 @@
 module Main where
 
-import Prelude hiding (lex)
-import System.Environment
-import Lexer
+import           Prelude
+import           System.Environment
 import Parser
 
 main :: IO ()
 main = do
-  prog <- getProgName
-  args <- getArgs
-  case args of
-    [_] -> pure ()
-    _ -> error $ "usage: " ++ prog ++ " filename"
-  src <- readFile (head args)
-  case lex (head args) src of
-    (Left err) -> error $ show err
-    (Right toks) -> do print toks
-                       print $ parse toks
+  src <- getContents
+  print <$> parse src
